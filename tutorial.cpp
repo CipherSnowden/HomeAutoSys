c48e3e9d51c579b47829fd9ad5d6f0122f1530cb
CipherSnowden <46859406+CipherSnowden@users.noreply.github.com>
2019-01-26 13:29:17 +0530

    Create tutorial.cpp

---
+++ tutorial.cpp
@@ -0,0 +1,22 @@
+/*
+ * Tutorial.cpp
+ *
+ * Created: 20-01-2019 21:59:33
+ * Author : Chetanya Saxena
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRB = 0b11111111;
+    while (1)
+    {
+	    for(int i=1; i<=256; i=i+i)
+	    {
+			PORTB = i;
+			_delay_ms(500);
+	    }
+    }
+}
